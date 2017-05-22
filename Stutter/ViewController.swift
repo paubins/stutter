0006cc7878942c6d25233c1db49073bb777d0fc5
paubins <paubin12@gmail.com>
2017-05-22 16:15:57 -0700

    Initial Commit

---
+++ Stutter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stutter
+//
+//  Created by Patrick Aubin on 5/22/17.
+//  Copyright © 2017 com.paubins.Stutter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
